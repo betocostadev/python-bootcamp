6639c9671630ffa07c11c6cfa86d3dec4204b758
betocostadev <robertomarx@gmail.com>
2024-04-12 19:52:24 -0300

    Started basics

---
+++ basics/data_types.py
@@ -0,0 +1,18 @@
+# String (str)
+print("==== Python basics ====")
+# Integer (int)
+print("Integer")
+print(10)
+print(-2)
+# Float (float) - Inference
+print("Float")
+num1 = 10
+num2 = 1.5
+print(num1 - num2)
+# Boolean (bool)
+print("Boolean")
+if (num1 - num2) == 8.5:
+    print(True)
+else:
+    print(False)
+    
