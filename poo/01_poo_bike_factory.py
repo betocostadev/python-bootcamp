00c7af0d0e0874b3030f0d87280ebe7ca757398c
betocostadev <robertomarx@gmail.com>
2024-04-30 14:41:06 -0300

    Added bike factory challenge

--- poo/01_poo_bike_factory.py
+++ poo/01_poo_bike_factory.py
@@ -3,3 +3,47 @@
 # Create a class Bike with the following attributes:
 # color, model, year, price and a method to get the bike info.
 # also the bikes must have the methods to horn, accelerate and brake.
+
+print("======== Python - Introduction to Object Oriented Programming in Python ========")
+print("\n=== Challenge: Bike Factory ===\n")
+
+
+class Bike:
+    def __init__(self, model, color, year, price) -> None:
+        self.model = model
+        self.color = color
+        self.year = year
+        self.price = price
+        self.is_horn = False
+        self.is_accelerating = False
+        self.is_braking = False
+
+    def horn(self):
+        self.is_horn = not self.is_horn
+        return "Honking..." if self.is_horn else "Horn stopped."
+
+    def accelerate(self):
+        self.is_accelerating = True
+        return "Accelerating..."
+
+    def brake(self):
+        self.is_braking = True
+        return "Braking..."
+
+    def get_info(self):
+        return f"Bike Information:\nModel: {self.model}, Color: {self.color}, Year: {self.year}, Price: {self.price}"
+
+    def __str__(self) -> str:
+        pass
+        return f"{self.__class__.__name__}: {', '.join([f'{key}: {value}' for key, value in self.__dict__.items()])}"
+
+
+b1 = Bike("B1", "Red", 2021, 1500)
+print(b1.model, b1.color)
+print(b1.get_info())
+print(b1.accelerate())
+print(b1.horn())  # Bike.horn(b1)
+print(b1.horn())
+print(b1.brake(), "\n")
+# Calling the __str__ method
+print(b1)
