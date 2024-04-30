412049506f0707be4c7814149dfeab98863e434f
betocostadev <robertomarx@gmail.com>
2024-04-30 11:28:25 -0300

    Starting poo

---
+++ poo/01_poo_intro.py
@@ -0,0 +1,6 @@
+# Python - Introduction to Object Oriented Programming in Python
+# Intro
+# Object Oriented Programming (OOP) is a programming paradigm that relies on the concept of classes and objects.
+# It is used to structure a software program into simple, reusable pieces of code blueprints (usually called classes),
+# which are used to create individual instances of objects.
+# There are many object-oriented programming languages including JavaScript, C++, Java, and Python.
