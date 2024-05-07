ff3335d65f7ab58c345dd4f758d321a44195f01a
betocostadev <robertomarx@gmail.com>
2024-05-07 14:30:49 -0300

    Added needed classes for oop banking

--- labs/banking_oop.py
+++ labs/banking_oop.py
@@ -3,7 +3,7 @@
 # This system is based in the baking2.py file.
 # The main difference is that we will use OOP concepts to create the system.
 
-# from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
 from datetime import datetime
 
 
@@ -99,23 +99,108 @@ class Account:
         return False
 
 
-class Transaction:
-    def __init__(self, account: Account, amount: float):
-        self.account = account
-        self.amount = amount
+class CheckingAccount(Account):
+    def __init__(self, number, client: Client, limit=500, withdrawal_limit_per_day=3):
+        super().__init__(number, client)
+        self.limit = limit
+        self._withdrawal_count_today = 0
+        self._withdrawal_limit_per_day = withdrawal_limit_per_day
+
+    def withdraw(self, amount):
+        exceed_limit = amount > self.limit
+        exceed_withdrawal_limit_per_day = self._withdrawal_count_today >= self._withdrawal_limit_per_day
+        # number_of_withdrawals_today =
+        # len([transaction for transaction in self.statements.transactions if transaction["type"] === Withdrawal])
+
+        if exceed_limit:
+            print("@@@ Operation failed! You don't have enough balance to withdraw this amount. @@@")
+
+        elif exceed_withdrawal_limit_per_day:
+            print("@@@ Operation failed! You have reached the withdrawal limit for today. @@@")
+
+        elif amount > 0:
+            self._withdrawal_count_today += 1
+            return super().withdraw(amount)
+
+        else:
+            print("@@@ Operation failed! Invalid amount. @@@")
+
+        return False
 
     def __str__(self) -> str:
-        return f"{self.__class__.__name__}: {', '.join([f'{key}: {value}' for key, value in self.__dict__.items()])}"
+        return f"""\
+            Agency: {self.agency}
+            Account: {self.number}
+            Balance: R${self.balance}
+            Limit: R${self.limit}
+            Owner: {self.client.name}
+            """
+
+
+class Transaction(ABC):
+    @property
+    def amount(self):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def register(self, account):
+        pass
 
 
 class Statements:
-    def __init__(self, account: Account, transactions: list = []):
-        self.account = account
-        self.transactions = transactions
+    def __init__(self):
+        self._transactions = []
+
+    @property
+    def transactions(self):
+        return self._transactions
 
     def add_statement(self, transaction: Transaction):
-        self.transactions.append(transaction)
+        self._transactions.append(
+            {
+                "type": transaction.__class__.__name__,
+                "amount": transaction.amount,
+                "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            }
+        )
         return f"Transaction {transaction} added"
 
     def check_statements(self):
         return self.transactions
+
+
+class Deposit(Transaction):
+    def __init__(self, amount):
+        self._amount = amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    def register(self, account: Account):
+        success = account.deposit(self.amount)
+
+        if success:
+            self._account.statements.add_statement(self)
+            return True
+        else:
+            return False
+
+
+class Withdrawal(Transaction):
+    def __init__(self, amount):
+        self._amount = amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    def register(self, account: Account):
+        success = account.withdraw(self.amount)
+
+        if success:
+            account.statements.add_statement(self)
+            return True
+        else:
+            return False
