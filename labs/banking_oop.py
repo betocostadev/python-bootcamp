fd667f4799bb333ec8ffb3ae01d74aca7a94b11a
betocostadev <robertomarx@gmail.com>
2024-05-07 12:15:17 -0300

    Started creating oop banking

--- labs/banking_oop.py
+++ labs/banking_oop.py
@@ -1,59 +1,84 @@
 # Create a command line application that simulates a simple bank account.
-# The application should have the following features:
-# Create user
-# Create account - Tie the account to a user
 
-# Users must be registered using the following fields
-# Name, date_birth, document, address
-# Address is a string and must have the following fields and in the format below:
-# Street, number - neighborhood - city/state
-# Only document number must be registered
-# Only one account per user (Document - CPF)
-# User can have more than one banking account - Account 1: 1, account 2: 2, etc.
-# Document is a string and must have only numbers
-
-# Account
-# Account number - must be generated automatically
-# All of the user accounts must be saved in a list
-# Balance - must be zero when the account is created
-# Statements - must be empty when the account is created
-# Withdrawal limit - must be 500
-# Withdrawal count - must be zero when the account is created
-# Withdrawal limit per day - must be 3
-# Account will have the following fields:
-# agency, account_number, user (document)
-# Account agency must be 0001
-
-# Other features:
-# Deposit money
-# Withdraw money
-# Check statements
-# The system will allow the user to withdraw 3 times a day with a maximum of 500 per withdrawal.
-
-# This system is based in the baking2.py file. 
+# This system is based in the baking2.py file.
 # The main difference is that we will use OOP concepts to create the system.
 
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+
 class Client:
-    def __init__(self, name: str, date_birth: str, document: str, address: str):
-        self.name = name
-        self.date_birth = date_birth
-        self.document = document
+    def __init__(self, address: str):
         self.address = address
+        self.accounts = []
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}: {', '.join([f'{key}: {value}' for key, value in self.__dict__.items()])}"
 
-    def __eq__(self, other) -> bool:
-        return self.document == other.document
+    def make_transaction(self, account, transaction):
+        transaction.register(account)
 
-    def __hash__(self) -> int:
-        return hash(self.document)
+    def add_account(self, account):
+        self.accounts.append(account)
+        return f"Account {account} added"
 
 
-user1 = Client("John Doe", "01/01/1990", "12345678901", "123 Main St - Downtown - New York/NY")
-user2 = Client("Jane Doe", "01/01/1995", "98765432101", "123 Main St - Downtown - New York/NY")
-print(user1)
-print(user2)
-print(user1 == user2)
-print(hash(user1))
-print(hash(user2))
+class Person(Client):
+    def __init__(self, name: str, date_birth: datetime, document: str, address: str, ):
+        super().__init__(address)
+        self.name = name
+        self.date_birth = date_birth
+        self.document = document
+
+    def __str__(self) -> str:
+        return f"{super().__str__()} - Address: {self.address}"
+
+
+class Account:
+    def __init__(self, number, client: Client):
+        self._balance = 0
+        self._number = number
+        self._agency = "0001"
+        self._client = client
+        self.statements = Statements()
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {', '.join([f'{key}: {value}' for key, value in self.__dict__.items()])}"
+
+    @classmethod
+    def new_account(cls, client: Client, number):
+        return cls(number, client)
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @property
+    def number(self):
+        return self._number
+
+    @property
+    def agency(self):
+        return self._agency
+
+
+class Transaction:
+    def __init__(self, account: Account, amount: float):
+        self.account = account
+        self.amount = amount
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {', '.join([f'{key}: {value}' for key, value in self.__dict__.items()])}"
+
+
+class Statements:
+    def __init__(self, account: Account, transactions: list = []):
+        self.account = account
+        self.transactions = transactions
+
+    def add_statement(self, transaction: Transaction):
+        self.transactions.append(transaction)
+        return f"Transaction {transaction} added"
+
+    def check_statements(self):
+        return self.transactions
