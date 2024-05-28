cf07d1c34d040c02640d9b0105990ab00751287e
betocostadev <robertomarx@gmail.com>
2024-05-28 15:49:52 -0300

    Added examples to file manipulation

--- using_files/00_files.py
+++ using_files/00_files.py
@@ -1,8 +1,8 @@
 # Python - Introduction to Object Oriented Programming in Python
-# Dealing with files
+# Manipulating files
 
 print("\n======== Python - Introduction to Object Oriented Programming in Python ========\n")
-print("======== Dealing with files ========")
+print("======== Manipulating files ========")
 
 # We will deal mainly with TXT and CSV files
 # Since they are the most common file types in data processing
@@ -18,6 +18,50 @@ print("======== Dealing with files ========")
 
 # Reading a file
 # file = open('file.txt', 'r')
-# content = file.read()
+# content = file.read() # Reads the entire file
+# content = file.readline()  # Reads the first line
+# content = file.readlines()  # Reads all lines and returns a list
 # print(content)
 # file.close()
+
+print("\nReading files\n")
+txt_file = open('using_files/example.txt', 'r')
+content = txt_file.readline()  # Can use an int to limit the number of characters read
+# It reads line by line, you can keep calling the function for the next lines
+print("Reading the file line by line: ")
+print(content)
+content = txt_file.readline()  # Will read the next line
+print(content)  # Will print the next line
+txt_file.close()
+
+txt_file = open('using_files/example.txt', 'r')
+content = txt_file.readlines()
+print("\nReading all lines of the file in list format: ")
+for line in content:
+    print(line)
+txt_file.close()
+
+print("\nReading the entire file: ")
+txt_file = open('using_files/example.txt', 'r')
+content = txt_file.read()
+print(content)
+txt_file.close()
+
+# Reading line by line and checking the end of the file
+print("\nIterating over the file content:")
+txt_file = open('using_files/example.txt', 'r')
+
+while len(line := txt_file.readline()):
+    print(line)
+
+txt_file.close()
+
+
+print("\nWriting files\n")
+# Writing a file
+new_txt_file = open('using_files/new_file.txt', 'w')
+
+new_txt_file.write("This is a new file.\n")
+new_txt_file.writelines(["This is the second line.\n", "This is the third line.\n"])
+new_txt_file.write("This is the fourth line.\n")
+new_txt_file.close()
