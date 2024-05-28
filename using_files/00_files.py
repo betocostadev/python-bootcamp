52595b51597bb537b77df21229218b7ffd03b414
betocostadev <robertomarx@gmail.com>
2024-05-28 12:40:54 -0300

    Started using external files

---
+++ using_files/00_files.py
@@ -0,0 +1,23 @@
+# Python - Introduction to Object Oriented Programming in Python
+# Dealing with files
+
+print("\n======== Python - Introduction to Object Oriented Programming in Python ========\n")
+print("======== Dealing with files ========")
+
+# We will deal mainly with TXT and CSV files
+# Since they are the most common file types in data processing
+
+# TXT files
+# Opening a file
+# file = open('file.txt', 'r')  # r - read mode
+# file = open('file.txt', 'w')  # w - write mode
+# file = open('file.txt', 'a')  # a - append mode
+# file = open('file.txt', 'r+')  # r+ - read and write mode
+# file = open('file.txt', 'w+')  # w+ - write and read mode
+# file = open('file.txt', 'a+')  # a+ - append and read mode
+
+# Reading a file
+# file = open('file.txt', 'r')
+# content = file.read()
+# print(content)
+# file.close()
