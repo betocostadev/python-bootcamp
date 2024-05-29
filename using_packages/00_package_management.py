522399f30e752078b2af0cf02f000834b4296589
betocostadev <robertomarx@gmail.com>
2024-05-29 17:08:20 -0300

    Started module on packages

---
+++ using_packages/00_package_management.py
@@ -0,0 +1,22 @@
+# Python - Learning about Packages
+# Managing packages
+
+print("\n======== Python - Learning about Packages ========\n")
+print("======== Managing packages ========")
+
+# Packages are a way to organize Python modules
+# They are directories with a special __init__.py file
+# This file can be empty, but it indicates that the directory is a Python package
+
+# To avoid package version conflicts and to manage dependencies, it is recommended to use a virtual environment
+
+# To create an use a virtual environment:
+# python3 -m venv .env
+# source .env/bin/activate
+# pip install package_name
+
+# To deactivate the virtual environment:
+# deactivate
+
+# To install a package:
+# pip install package_name
