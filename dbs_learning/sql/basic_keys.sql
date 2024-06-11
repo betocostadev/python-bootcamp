6724ff1fcb99da235ab282e111ac692322debb0e
betocostadev <robertomarx@gmail.com>
2024-06-11 15:39:12 -0300

    Added some more commands to travelling db

--- dbs_learning/sql/basic_keys.sql
+++ dbs_learning/sql/basic_keys.sql
@@ -32,6 +32,59 @@ CREATE TABLE reservations (
 
 -- Modifying the tables to use the primary keys and foreign keys
 
+-- Modifying the users table to use the primary key
 ALTER TABLE users
 MODIFY COLUMN id INT AUTO_INCREMENT,
 ADD PRIMARY KEY (id);
+
+-- Modifying the destinations table to use the primary key
+ALTER TABLE destinations
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Modifying the reservations table to use the primary key and foreign keys
+ALTER TABLE reservations
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id),
+ADD FOREIGN KEY (user_id) REFERENCES users(id),
+ADD FOREIGN KEY (destination_id) REFERENCES destinations(id);
+
+-- Another way by adding the constraints separately
+ALTER TABLE reservations
+ADD CONSTRAINT fk_reservations_destinations
+FOREIGN KEY (destination_id) REFERENCES destinations (id);
+
+ALTER TABLE reservations
+ADD CONSTRAINT fk_reservations_users
+FOREIGN KEY (user_id) REFERENCES users (id);
+
+-- Or also adding a cascade delete to the foreign key
+ALTER TABLE reservations
+ADD CONSTRAINT fk_reservations_users
+FOREIGN KEY (user_id) REFERENCES users (id)
+ON DELETE CASCADE;
+
+-- Changing the reservations table and adding a command to delete the reservation if the user is deleted
+ALTER TABLE reservations
+ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+-- Doing the same for the destination
+ALTER TABLE reservations
+ADD FOREIGN KEY (destination_id) REFERENCES destinations(id) ON DELETE CASCADE;
+
+-- Inserting data
+
+INSERT INTO users (name, email, address, birth_date) VALUES ('Jane Doe', 'jane_doe@icloud.com', 'Street Bunglaros, 220 - Brookling New York/NY', '1989-03-15');
+INSERT INTO reservations (user_id, destination_id, reservation_date, reservation_status) VALUES (5, 2, '2024-10-05', 'PENDING');
+INSERT INTO reservations (user_id, destination_id, reservation_date, reservation_status) VALUES (2, 2, '2024-10-05', 'PENDING');
+INSERT INTO destinations (name, description) VALUES ('Ubatuba', 'Lots of beaches to have a lot of fun!');
+INSERT INTO destinations (name, description) VALUES ('Monte Verde', 'Great place to relax and enjoy the nature');
+INSERT INTO reservations (user_id, destination_id, reservation_date, reservation_status) VALUES (6, 7, '2024-07-15', 'CANCELLED');
+
+-- Dropping a constraint
+ALTER TABLE reservations
+DROP CONSTRAINT fk_reservations_users;
+
+-- Dropping tables
+
+DROP TABLE users;
+
