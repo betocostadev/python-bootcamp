2bb5f1869cf6945fa769d5953ea480d3baa1f781
betocostadev <robertomarx@gmail.com>
2024-07-10 19:37:41 -0300

    Feat: Added tests and more use cases for products controller

--- tdd_project/store/controllers/product.py
+++ tdd_project/store/controllers/product.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Body, Depends, status
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, status
+from pydantic import UUID4
+from store.core.exceptions import NotFoundException
 from store.schemas.product import ProductIn, ProductOut
 from store.usecases.product import ProductUsecase
 
@@ -13,6 +15,11 @@ async def create_product(
     return await usecase.create(body=body)
 
 
-@router.get("/products/", tags=["products"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+@router.get("/products/{id}", response_model=ProductOut, status_code=status.HTTP_200_OK)
+async def get_product(
+    id: UUID4 = Path(alias="id"), usecase: ProductUsecase = Depends()
+) -> ProductOut:
+    try:
+        return await usecase.get(id=id)
+    except NotFoundException as err:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=err.message)
--- tdd_project/tests/controllers/test_product.py
+++ tdd_project/tests/controllers/test_product.py
@@ -5,8 +5,34 @@ from fastapi import status
 async def test_controllers_should_create_product(client, products_url):
     response = await client.post(products_url, json=product_data_factory())
 
+    content = response.json()
+    del content["id"]
+    del content["created_at"]
+    del content["updated_at"]
+
     assert response.status_code == status.HTTP_201_CREATED
-    # assert response.json() == product_data_factory()
+    assert content == product_data_factory()
 
 
-# async def test_controller_should_get_products
+async def test_controllers_should_get_product_by_id(
+    client, products_url, product_inserted
+):
+    response = await client.get(f"{products_url}{product_inserted.id}")
+    content = response.json()
+
+    del content["created_at"]
+    del content["updated_at"]
+
+    assert response.status_code == status.HTTP_200_OK
+    assert content["id"] == str(product_inserted.id)
+
+
+async def test_controllers_should_get_product_not_found(client, products_url):
+    response = await client.get(f"{products_url}1e4f214e-85f7-461a-89d0-a751a32e3bb9")
+
+    content = response.json()
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert content == {
+        "detail": "Product not found with filter: 1e4f214e-85f7-461a-89d0-a751a32e3bb9"
+    }
